4aec0d20369b1024ed66c32477a83eafda4aa9fd
Lucas Belin <lucasbelinpro@gmail.com>
2023-02-07 17:24:30 +0100

    add temporary qrcode generation

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,8 +1,13 @@
+import QRCode from "react-qr-code"
+
 function Login() {
   return (
     <>
       <img src="/src/assets/logo.png"></img>
       <div>Login</div>
+      <div id="qrcode">
+        <QRCode value="http://192.168.56.1:3000/" />
+      </div>
     </>
   )
 }
