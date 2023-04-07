c77ffc2492b65f0cfd5b5936fd83584b15dba899
Lucas Belin <lucasbelinpro@gmail.com>
2023-04-07 20:02:30 +0200

    feat: add login page

---
+++ lib/login_page.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_practice/products_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 70),
+            Container(
+              width: 170,
+              height: 170,
+              padding: const EdgeInsets.all(30),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(500),
+              ),
+              child: const Center(
+                  child: Image(image: AssetImage("images/logo.png"))),
+            ),
+            const SizedBox(height: 20),
+            Container(
+              padding: const EdgeInsets.symmetric(horizontal: 40),
+              child: const Text(
+                "To login please scan the QR Code you received in your email.",
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(height: 20),
+            const Image(image: AssetImage("images/qr_code.png")),
+            InkWell(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const ProductsPage()),
+                );
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Colors.white,
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 10),
+                  child: IntrinsicWidth(
+                    child: Row(
+                      children: const [
+                        Icon(
+                          Icons.qr_code, // Replace with the desired icon
+                          color: Colors.white,
+                        ),
+                        SizedBox(width: 5), // Add a SizedBox for spacing
+                        Text(
+                          "Scan QR Code",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'login_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -15,21 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Main(),
-    );
-  }
-}
-
-class Main extends StatelessWidget {
-  const Main({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Paye ton kawa"),
-      ),
-      body: const Text("Paye ton kawa page d'accueil"),
+      home: const LoginPage(),
     );
   }
 }
